55ba31c4f652ab5b6f0b96bdd154115510e055a8
amrit115 <amritbanjara2030@gmail.com>
2016-07-21 16:46:12 +0545

    widget registering in Wordpress backend

--- functions.php
+++ functions.php
@@ -10,4 +10,54 @@ function amrit_enqueue_styles() {
     );
 }
 add_action( 'wp_enqueue_scripts', 'amrit_enqueue_styles' );
-?>
+
+/* add widget functions */
+function child_theme_widgets_init() {
+ 
+    // First footer widget area, located in the footer. Empty by default.
+    register_sidebar( array(
+        'name' => __( 'First Footer Widget Area', 'childtheme' ),
+        'id' => 'first-footer-widget-area',
+        'description' => __( 'The is a first footer widget area', 'childtheme' ),
+        'before_widget' => '<div id="%1$s" class="widget-container %2$s">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>',
+    ) );
+ 
+    // Second Footer Widget Area, located in the footer. Empty by default.
+    register_sidebar( array(
+        'name' => __( 'Second Footer Widget Area', 'childtheme' ),
+        'id' => 'second-footer-widget-area',
+        'description' => __( 'The is a second footer widget area', 'childtheme' ),
+        'before_widget' => '<div id="%1$s" class="widget-container %2$s">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>',
+    ) );
+ 
+    // Third Footer Widget Area, located in the footer. Empty by default.
+    register_sidebar( array(
+        'name' => __( 'Third Footer Widget Area', 'childtheme' ),
+        'id' => 'third-footer-widget-area',
+        'description' => __( 'The is a third footer widget area', 'childtheme' ),
+        'before_widget' => '<div id="%1$s" class="widget-container %2$s">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>',
+    ) );
+ 
+    // Fourth Footer Widget Area, located in the footer. Empty by default.
+    register_sidebar( array(
+        'name' => __( 'Fourth Footer Widget Area', 'childtheme' ),
+        'id' => 'fourth-footer-widget-area',
+        'description' => __( 'The is a fourth footer widget area', 'childtheme' ),
+        'before_widget' => '<div id="%1$s" class="widget-container %2$s">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>',
+    ) );
+         
+}
+ 
+add_action( 'widgets_init', 'child_theme_widgets_init' );
