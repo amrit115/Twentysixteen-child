4e41ccd8fcbc5403c0867f02cc2b7e99bb228308
amrit115 <amritbanjara2030@gmail.com>
2016-07-26 16:14:59 +0545

    footer.php file modified

--- footer.php
+++ footer.php
@@ -56,6 +56,20 @@
 				?>
 				<span class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span>
 				<a href="<?php echo esc_url( __( 'https://amritbanjara.com.np/', 'twentysixteen' ) ); ?>"><?php printf( __( 'Designed By: %s', 'twentysixteen' ), 'Amrit Banjara' ); ?></a>
+			<?php if ( has_nav_menu( 'social' ) ) : ?>
+				<nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'twentysixteen' ); ?>">
+					<?php
+						wp_nav_menu( array(
+							'theme_location' => 'social',
+							'menu_class'     => 'social-links-menu',
+							'depth'          => 1,
+							'link_before'    => '<span class="screen-reader-text">',
+							'link_after'     => '</span>',
+						) );
+					?>
+				</nav><!-- .social-navigation -->
+			<?php endif; ?>
+
 			</div><!-- .site-info -->
 		</footer><!-- .site-footer -->
 	</div><!-- .site-inner -->
